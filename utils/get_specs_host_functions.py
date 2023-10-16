31ac478fc6e540b1926890ffa5de2897e8194377
neithanmo <neithanmo@gmail.com>
2023-10-16 14:58:13 -0600

    Add regex to include host-api function parameters and return types, so we can later compare API

--- utils/get_specs_host_functions.py
+++ utils/get_specs_host_functions.py
@@ -63,3 +63,4 @@ if response.status_code == 200:
     print(f"{len(functions)} host-api function specifications saved to {SPECS_HOST_API_NAME}")
 else:
     print("Failed to retrieve the markdown file")
+
