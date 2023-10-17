13769a6f9c5948beec098eb313f80f7d71aa6f87
neithanmo <neithanmo@gmail.com>
2023-10-17 15:26:33 -0600

    minor improvements to get specification for host-api

--- utils/get_specs_host_functions.py
+++ utils/get_specs_host_functions.py
@@ -7,60 +7,57 @@ URL = "https://raw.githubusercontent.com/w3f/polkadot-spec/main/docs/chap-host-a
 SPECS_HOST_API_NAME = "scripts/host_api/specs_host_api.json"
 
 response = requests.get(URL)
-
-def parse_types(segment):
-    # segment is of type: 
-    # (param $root i32) (param $proof i64)
-    # (param $key i64) (param $value i64)
-    # we need to remove param and $key
-    cleaned_segment = re.sub(r'\bparam\b|\$\w+', '', segment)
-    types = re.findall(r'\b[a-zA-Z0-9_]+\b', cleaned_segment)
-
-    return types
-
-def parse_result_types(segment):
-    # Remove the 'result' keyword from the segment, then parse the types
-    cleaned_segment = re.sub(r'\bresult\b', '', segment)
-    types = re.findall(r'\b[a-zA-Z0-9_]+\b', cleaned_segment)
-    return types
-
+response_text = {}
 
 if response.status_code == 200:
-    markdown_text = response.text
+    response_text = response.text
 
-    # The main pattern matches each function definition, 
-    # capturing the entire parameters and results segments
-    pattern = re.compile(r'\(func \$(\w+)((?:(?:\s|\n)*\(param[^\)]*\))*)((?:(?:\s|\n)*\(result[^\)]*\))*)', re.MULTILINE | re.DOTALL)
-    
-    functions = []
-    for match in pattern.finditer(markdown_text):
-        func_name = match.group(1)
+# segment is of type: 
+# (param $root i32) (param $proof i64)
+# (param $key i64) (param $value i64)
+# we need to remove param and $key
+def parse_types(segment):
+    cleaned_segment = re.sub(r'\bparam\b|\$\w+', '', segment)
+    types = re.findall(r'\b[a-zA-Z0-9_]+\b', cleaned_segment)
+    return types
 
-        # Extract parameters and results
-        params_segment = match.group(2)
-        results_segment = match.group(3)
+# Adjusted function to parse result types
+def parse_result_types(segment):
+    # Clean the segment by removing 'result' or 'return'
+    cleaned_segment = re.sub(r'\((result|return)\s*', '', segment).strip()
+    types = re.findall(r'\b[a-zA-Z0-9_]+\b', cleaned_segment)
+    return types
 
-        params = []
-        if params_segment:
-            params = parse_types(params_segment)
+# The main regex to capture the function names, params and return types 
+pattern = re.compile(r'\(func \$(\w+)((?:(?:\s|\n)*\(param[^\)]*\))*)((?:(?:\s|\n)*\((result|return)[^\)]*\))*)', re.MULTILINE | re.DOTALL)
 
-        results = []
-        if results_segment:
-            results = parse_result_types(results_segment)
 
-        function = {
-            "name": func_name,
-            "params": params,
-            "results": results
-        }
+functions = []
+for match in pattern.finditer(response_text):
+    func_name = match.group(1)
 
-        functions.append(function)
+    # Extract parameters and results
+    params_segment = match.group(2)
+    results_segment = match.group(3)
 
-    # Save function information to a JSON file
-    with open(SPECS_HOST_API_NAME, "w") as file:
-        json.dump(functions, file, indent=4)
+    params = []
+    if params_segment:
+        params = parse_types(params_segment)
 
-    print(f"{len(functions)} host-api function specifications saved to {SPECS_HOST_API_NAME}")
-else:
-    print("Failed to retrieve the markdown file")
+    results = []
+    if results_segment:
+        results = parse_result_types(results_segment)
 
+    function = {
+        "name": func_name,
+        "params": params,
+        "results": results
+    }
+
+    functions.append(function)
+
+# Save function information to a JSON file
+with open(SPECS_HOST_API_NAME, "w") as file:
+    json.dump(functions, file, indent=4)
+
+print(f"{len(functions)} host-api function specifications saved to {SPECS_HOST_API_NAME}")
